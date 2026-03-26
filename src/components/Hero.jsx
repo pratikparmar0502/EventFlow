3a49ccc518172580facfc6eacc6c44eec7926d7e
pratik parmar <pratikparmar0502@gmail.com>
2026-03-26 17:39:38 +0530

    hero half

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
-import { Box } from "@mui/material";
+import { Box, Chip, Stack, TextField, Typography } from "@mui/material";
 import React from "react";
 import heroImg from "../assets/images/hero_img.jpg";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Hero = () => {
+  const chipEvents = [
+    "All",
+    "Tech",
+    "Music",
+    "Sports",
+    "Food",
+    "Gaming",
+    "Networking",
+  ];
   return (
     <>
       <Box
@@ -12,9 +22,70 @@ const Hero = () => {
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           minHeight: "80vh",
+          position: "relative",
+          color: "#F5F5F5",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Box sx={{ color: "white" }}></Box>
+        <Box sx={{ zIndex: 1, fontFamily: "var(--primary-font)" }}>
+          <Box
+            sx={{
+              fontFamily: "var(--primary-font)",
+              fontSize: { xs: "2.7rem", sm: "3rem", md: "4rem", lg: "4.5rem" },
+              textAlign: "center",
+              padding: "10px 15px",
+              fontWeight: "700",
+            }}
+          >
+            Discover Unforgettable Events
+          </Box>
+          <Box
+            sx={{
+              fontFamily: "var(--primary-font)",
+              color: "#D2D2D2",
+              textAlign: "center",
+              fontWeight: "400",
+              padding: "5px 15px",
+              //   width: { xs: "100%", md: "800px" },
+              fontSize: { xs: "1.1rem", md: "1.2rem" },
+            }}
+          >
+            Find and book the best experience near you - concerts, conferneces,
+            games and more.
+          </Box>
+          {/* <Box> */}
+          {/* <SearchIcon></SearchIcon> */}
+          {/* <TextField
+            {...params}
+            label="Search input"
+            slotProps={{
+              input: {
+                ...params.InputProps,
+                type: "search",
+              },
+            }}
+          /> */}
+          {/* </Box> */}
+          {/* <Stack spacing={2} direction="row" justifyContent={"center"}>
+            {chipEvents.map((item) => (
+              <Chip
+                label={item}
+                variant="outlined"
+                sx={{
+                  backgroundColor: "#F5F5F5",
+                  fontSize: "1.1rem",
+                  fontWeight: "600",
+                  padding: "18px 10px",
+                  borderRadius: "50px",
+                  fontFamily: "var(--primary-font)",
+                }}
+              ></Chip>
+            ))}
+          </Stack> */}
+        </Box>
       </Box>
     </>
   );
